4d3656a28609f1860ad1931ac4791b4c79696157
AlexLyave <antinlay@gmail.com>
2022-03-08 00:38:33 +0300

    1. Прописаны ошибки гостей в методах ПОСТ и ГЕТ 2. Добавлены статусы 500 - ошибка и 200 - ок 3. Поиск и преобразование гостей в массив 4. Добавлены 3 пользователя

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express()
 
 let db, guests, points
 
+app.use(express.json())
+
 mongo.connect(
   url,
   {
@@ -23,10 +25,27 @@ mongo.connect(
   }
 )
 app.post("/guest", (req, res) => {
-  /* */
+  const name = req.body.name
+  guests.insertOne({ name: name}, (err, result) =>
+  {
+    if (err) {
+      console.error(err)
+      res.status(500).json({ err: err })
+      return
+    }
+    console.log(result)
+    res.status(200).json({ ok: true})
+  })
 })
 app.get("/guests", (req, res) => {
-  /* */
+  guests.find().toArray((err, items) => {
+    if (err) {
+      console.error(err)
+      res.status(500).json({ err: err })
+      return
+    }
+    res.status(200).json({ trips: items })
+  })
 })
 app.post("/point", (req, res) => {
   /* */
