98778ba8fb4e76e800e8b1b4121e3d81d9d4be79
AlexLyave <antinlay@gmail.com>
2022-03-07 23:58:18 +0300

    1. Заготовка для запросов ПОСТ и ГЕТ для гостей 2. Заготовка для запросов ПОСТ и ГЕТ для баллов на картах гостей 3. Написан метод запуска сервера

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const mongo = require("mongodb").MongoClient
+
+const app = express()
+
+app.post("/guest", (req< res) => {
+  /* */
+})
+app.get("/guests", (req, res) => {
+  /* */
+})
+app.post("/point", (req, res) => {
+  /* */
+})
+app.get("/points", (req, res) => {
+  /* */
+})
+
+app.listen(3000, () => console.log("Server ready!"))
