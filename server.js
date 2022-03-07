d246257aacbf22dfb5995f0008f471f639f5d12a
AlexLyave <antinlay@gmail.com>
2022-03-08 00:16:27 +0300

    1. Подключились к mongodb

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 const express = require("express")
 const mongo = require("mongodb").MongoClient
+const url = "mongodb://127.0.0.1:27017/:27017"
 
 const app = express()
 
+let db, guests, points
+
+mongo.connect(
+  url,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    db = client.db("cardpoints")
+    guests = db.collection("guests")
+    points = db.collection("points")
+  }
+)
 app.post("/guest", (req, res) => {
   /* */
 })
